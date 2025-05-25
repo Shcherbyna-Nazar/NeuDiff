daab92d31bf5cb921acf842efd19c72d7997e0d8
Nazar <nazar.force@gmail.com>
2025-05-25 14:33:21 +0200

    prepare notebook

--- src/MyDiffMLP.jl
+++ src/MyDiffMLP.jl
@@ -2,10 +2,12 @@ module MyDiffMLP
 
 include("MyAD.jl")
 include("MyNN.jl")
+include("data_prep.jl")
 
 using .MyAD
 using .MyNN
+using .PrepareData  
 
-export MyAD, MyNN
+export MyAD, MyNN, prepare_dataset 
 
 end
